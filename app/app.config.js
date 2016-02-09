149deca35ebb193feb31a749d0306a1cb2694da1
hanky <menshakov.artem@gmail.com>
2016-02-09 22:48:04 +0300

    new release

--- app/app.config.js
+++ app/app.config.js
@@ -7,7 +7,7 @@
             //
             // For any unmatched url, redirect to /login
             $urlRouterProvider.otherwise("/login");
-            $urlRouterProvider.when('index.html', "/login");
+            //$urlRouterProvider.when('index.html', "/login");
 
         });
 })();
